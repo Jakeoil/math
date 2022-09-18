ff89679d38804470ebf2e2daca5aeb62d4680342
Jakeoil <jakeoil@bellsouth.net>
2022-09-18 17:28:50 -0400

    I guess I added the gui

--- rhombohedra.js
+++ rhombohedra.js
@@ -55,6 +55,16 @@ function threeTest() {
     // Shows up oddly.
     document.getElementById("three-test").appendChild(stats.dom);
 
+    const gui = new GUI();
+    const cubeFolder = gui.addFolder("Cube");
+    cubeFolder.add(cube.scale, "x", -5, 5);
+    cubeFolder.add(cube.scale, "y", -5, 5);
+    cubeFolder.add(cube.scale, "z", -5, 5);
+    cubeFolder.open();
+    const cameraFolder = gui.addFolder("Camera");
+    cameraFolder.add(camera.position, "z", 0, 10);
+    cameraFolder.open();
+
     var animate = function () {
         requestAnimationFrame(animate);
 
