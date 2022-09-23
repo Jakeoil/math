d6118d3cfc22a00ffa0ba96ac31a2c0014a3f97c
Jakeoil <jakeoil@bellsouth.net>
2022-09-23 16:00:56 -0400

    Learning threejs

--- rhombohedra.js
+++ rhombohedra.js
@@ -10,11 +10,7 @@ window.addEventListener("load", threeTest, false);
  */
 
 function threeTest() {
-    console.log(
-        `Three test for window: ${window.innerWidth}, ${window.innerHeight}`
-    );
-    console.log(THREE);
-
+    solarSystem();
     let controls;
     let stats;
 
@@ -25,7 +21,6 @@ function threeTest() {
     const cube = cubeGeometry();
     const line = lineGeometry();
     const cone = coneGeometry();
-    console.log(cone);
     scene.add(cube);
     scene.add(line);
     scene.add(cone);
@@ -33,8 +28,6 @@ function threeTest() {
     const light = new THREE.DirectionalLight(0xffffff, 1.5);
     light.position.set(-1, 2, 4);
     scene.add(light);
-
-    console.log(scene);
 
     window.addEventListener(
         "resize",
@@ -99,7 +92,7 @@ function init() {
     const scene = new THREE.Scene();
 
     const renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setSize(window.innerWidth / 2, window.innerHeight / 2);
     container.appendChild(renderer.domElement);
     return { container, camera, scene, renderer };
 }
@@ -112,8 +105,6 @@ function lineGeometry() {
     points.push(new THREE.Vector3(0, 10, 0));
     points.push(new THREE.Vector3(10, 0, 0));
     const geometry = new THREE.BufferGeometry(points);
-    console.log(`geometry: ${geometry}`);
-    console.log(geometry);
     const line = new THREE.Line(geometry, material);
     return line;
 }
@@ -133,233 +124,84 @@ function coneGeometry() {
     const cone = new THREE.Mesh(geo, material);
     return cone;
 }
+
 /**
- * This is trash.
- * @returns
+ * Just do the solar system in the book
  */
-function rhombohedraApp() {
-    console.log(`started`);
-    const canvas = document.querySelector("#gltest");
-    // Initialize the GL context
-    const gl = canvas.getContext("webgl");
+function solarSystem() {
+    //const canvas = document.createElement("canvas");
+    const canvas = document.querySelector("#solar-system");
+    const renderer = new THREE.WebGLRenderer({ canvas });
 
-    // Only continue if WebGL is available and working
-    if (gl === null) {
-        alert(
-            "Unable to initialize WebGL. Your browser or machine may not support it."
-        );
-        return;
+    const fov = 40;
+    const aspect = 2; // the canvas default
+    const near = 0.1;
+    const far = 1000;
+
+    const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    camera.position.set(0, 150, 0);
+    camera.up.set(0, 0, 1);
+    camera.lookAt(0, 0, 0);
+    const scene = new THREE.Scene();
+
+    const objects = [];
+
+    // One sphere for all
+    const radius = 1;
+    const wSegs = 6;
+    const hSegs = 6;
+    const sphereGeometry = new THREE.SphereGeometry(radius, wSegs, hSegs);
+
+    const sunMaterial = new THREE.MeshPhongMaterial({ emissive: 0xffff00 });
+    const sunMesh = new THREE.Mesh(sphereGeometry, sunMaterial);
+    sunMesh.scale.set(5, 5, 5);
+    scene.add(sunMesh);
+    objects.push(sunMesh);
+
+    const color = 0xffffff;
+    const intensity = 3;
+    const light = new THREE.PointLight(color, intensity);
+    scene.add(light);
+
+    const earthMaterial = new THREE.MeshPhongMaterial({
+        color: 0x2233ff,
+        emissive: 0x112244,
+    });
+    const earthMesh = new THREE.Mesh(sphereGeometry, earthMaterial);
+
+    earthMesh.position.x = 10;
+    //scene.add(earthMesh);
+    sunMesh.add(earthMesh);
+    objects.push(earthMesh);
+
+    function resizeRendererToDisplaySize(renderer) {
+        const canvas = renderer.domElement;
+        const width = canvas.clientWidth;
+        const height = canvas.clientHeight;
+        const needResize = canvas.width !== width || canvas.height !== height;
+        if (needResize) {
+            renderer.setSize(width, height, false);
+        }
+        return needResize;
     }
 
-    // Set clear color to black, fully opaque
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
-    // Clear the color buffer with specified clear color
-    gl.clear(gl.COLOR_BUFFER_BIT);
+    function render(time) {
+        time *= 0.001;
 
-    const vsSource = `
-    attribute vec4 aVertexPosition;
+        if (resizeRendererToDisplaySize(renderer)) {
+            const canvas = renderer.domElement;
+            camera.aspect = canvas.clientWidth / canvas.clientHeight;
+            camera.updateProjectionMatrix();
+        }
 
-    uniform mat4 uModelViewMatrix;
-    uniform mat4 uProjectionMatrix;
+        objects.forEach((obj) => {
+            obj.rotation.y = time;
+        });
 
-    void main() {
-      gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
-    }
-  `;
-    const fsSource = `
-    void main() {
-      gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
-    }
-  `;
+        renderer.render(scene, camera);
 
-    const shaderProgram = initShaderProgram(gl, vsSource, fsSource);
-    const programInfo = {
-        program: shaderProgram,
-        attribLocations: {
-            vertexPosition: gl.getAttribLocation(
-                shaderProgram,
-                "aVertexPosition"
-            ),
-        },
-        uniformLocations: {
-            projectionMatrix: gl.getUniformLocation(
-                shaderProgram,
-                "uProjectionMatrix"
-            ),
-            modelViewMatrix: gl.getUniformLocation(
-                shaderProgram,
-                "uModelViewMatrix"
-            ),
-        },
-    };
-
-    initBuffers(gl);
-    console.log(`called init buffers`);
-    drawScene(gl, programInfo);
-}
-
-//
-// Initialize a shader program, so WebGL knows how to draw our data
-//
-function initShaderProgram(gl, vsSource, fsSource) {
-    const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
-    const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
-
-    // Create the shader program
-
-    const shaderProgram = gl.createProgram();
-    gl.attachShader(shaderProgram, vertexShader);
-    gl.attachShader(shaderProgram, fragmentShader);
-    gl.linkProgram(shaderProgram);
-
-    // If creating the shader program failed, alert
-
-    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-        alert(
-            `Unable to initialize the shader program: ${gl.getProgramInfoLog(
-                shaderProgram
-            )}`
-        );
-        return null;
+        requestAnimationFrame(render);
     }
 
-    return shaderProgram;
-}
-
-//
-// creates a shader of the given type, uploads the source and
-// compiles it.
-//
-function loadShader(gl, type, source) {
-    const shader = gl.createShader(type);
-
-    // Send the source to the shader object
-
-    gl.shaderSource(shader, source);
-
-    // Compile the shader program
-
-    gl.compileShader(shader);
-
-    // See if it compiled successfully
-
-    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-        alert(
-            `An error occurred compiling the shaders: ${gl.getShaderInfoLog(
-                shader
-            )}`
-        );
-        gl.deleteShader(shader);
-        return null;
-    }
-
-    return shader;
-}
-function initBuffers(gl) {
-    // Create a buffer for the square's positions.
-
-    const positionBuffer = gl.createBuffer();
-
-    // Select the positionBuffer as the one to apply buffer
-    // operations to from here out.
-
-    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-
-    // Now create an array of positions for the square.
-
-    const positions = [1.0, 1.0, -1.0, 1.0, 1.0, -1.0, -1.0, -1.0];
-
-    // Now pass the list of positions into WebGL to build the
-    // shape. We do this by creating a Float32Array from the
-    // JavaScript array, then use it to fill the current buffer.
-
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
-
-    return {
-        position: positionBuffer,
-    };
-}
-function drawScene(gl, programInfo, buffers) {
-    gl.clearColor(0.0, 0.0, 0.0, 1.0); // Clear to black, fully opaque
-    gl.clearDepth(1.0); // Clear everything
-    gl.enable(gl.DEPTH_TEST); // Enable depth testing
-    gl.depthFunc(gl.LEQUAL); // Near things obscure far things
-
-    // Clear the canvas before we start drawing on it.
-
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-    // Create a perspective matrix, a special matrix that is
-    // used to simulate the distortion of perspective in a camera.
-    // Our field of view is 45 degrees, with a width/height
-    // ratio that matches the display size of the canvas
-    // and we only want to see objects between 0.1 units
-    // and 100 units away from the camera.
-
-    const fieldOfView = (45 * Math.PI) / 180; // in radians
-    const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
-    const zNear = 0.1;
-    const zFar = 100.0;
-    const projectionMatrix = mat4.create();
-
-    // note: glmatrix.js always has the first argument
-    // as the destination to receive the result.
-    mat4.perspective(projectionMatrix, fieldOfView, aspect, zNear, zFar);
-
-    // Set the drawing position to the "identity" point, which is
-    // the center of the scene.
-    const modelViewMatrix = mat4.create();
-
-    // Now move the drawing position a bit to where we want to
-    // start drawing the square.
-
-    mat4.translate(
-        modelViewMatrix, // destination matrix
-        modelViewMatrix, // matrix to translate
-        [-0.0, 0.0, -6.0]
-    ); // amount to translate
-
-    // Tell WebGL how to pull out the positions from the position
-    // buffer into the vertexPosition attribute.
-    {
-        const numComponents = 2; // pull out 2 values per iteration
-        const type = gl.FLOAT; // the data in the buffer is 32bit floats
-        const normalize = false; // don't normalize
-        const stride = 0; // how many bytes to get from one set of values to the next
-        // 0 = use type and numComponents above
-        const offset = 0; // how many bytes inside the buffer to start from
-        gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
-        gl.vertexAttribPointer(
-            programInfo.attribLocations.vertexPosition,
-            numComponents,
-            type,
-            normalize,
-            stride,
-            offset
-        );
-        gl.enableVertexAttribArray(programInfo.attribLocations.vertexPosition);
-    }
-
-    // Tell WebGL to use our program when drawing
-
-    gl.useProgram(programInfo.program);
-
-    // Set the shader uniforms
-
-    gl.uniformMatrix4fv(
-        programInfo.uniformLocations.projectionMatrix,
-        false,
-        projectionMatrix
-    );
-    gl.uniformMatrix4fv(
-        programInfo.uniformLocations.modelViewMatrix,
-        false,
-        modelViewMatrix
-    );
-
-    {
-        const offset = 0;
-        const vertexCount = 4;
-        gl.drawArrays(gl.TRIANGLE_STRIP, offset, vertexCount);
-    }
+    requestAnimationFrame(render);
 }
